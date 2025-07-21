59904a649a51c471e81adbde7ab4c783b68da361
Wilhelm Ågren <wilhelmagren98@gmail.com>
2025-07-21 11:47:09 +0200

    fix: cargo fmt with conform

--- .config/nvim/lua/formatter.lua
+++ .config/nvim/lua/formatter.lua
@@ -1,22 +1,22 @@
 require('conform').setup({
-    formatters_by_ft = {
-        python = { "ruff_format" },
-        rust = { "cargo_fmt" },
+  formatters_by_ft = {
+    python = { "ruff_format" },
+    rust = { "cargo_fmt" },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+  formatters = {
+    ruff_format = {
+      command = "ruff",
+      args = { "format", "-" },
+      stdin = true,
     },
-    format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
+    cargo_fmt = {
+      command = "cargo",
+      args = { "fmt", "--", "--emit=files" },
+      stdin = false,
     },
-    formatters = {
-        ruff_format = {
-            command = "ruff",
-            args = { "format", "-"  },
-            stdin = true,
-        },
-        cargo_fmt = {
-            command = "cargo",
-            args = { "fmt", "--", "-" },
-            stdin = true,
-        },
-    },
+  },
 })
