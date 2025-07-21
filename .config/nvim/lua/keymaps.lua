d23e99a7d50fe7cb98cbf75383c7cc6ee49f00cc
Wilhelm Ågren <wilhelmagren98@gmail.com>
2025-07-21 11:32:40 +0200

    feat: fzf config in keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,4 +2,10 @@ local keymap = vim.keymap.set
 local telescope_builtin = require('telescope.builtin')
 
 keymap('i', 'jk', '<Esc>', { noremap = false, desc = 'quick escape with "jk"' })
-keymap('n', '<C-f>', telescope_builtin.find_files, { desc = 'fuzzy find from cwd' })
+keymap('n', '<C-f>', function()
+    telescope_builtin.find_files({
+      hidden = true,
+    })
+  end,
+  { desc = 'fuzzy find from cwd' }
+)
